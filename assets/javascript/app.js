1e13df3936abcda87202b08e5147625856158459
Marina Delkova-Moro <marinadelkovamoro@gmail.com>
2019-05-30 15:55:18 -0700

    added game logic pseudo code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,14 @@
 console.log("This works");
+
+
+// When the user clicks on the start button, a 3-minute countdown starts and it is displayed on the page
+
+// The game ends when the time runs out. 
+// The page reveals the number of questions that player answered correctly and incorrectly.
+
+// To check if the answers that the player has given are matching the correct answers, compare them to the answers key [maybe an array?]
+
+// Count how many correct answers, wrong answers, and unanswered questions has the player given.
+
+// Display the result.
+
